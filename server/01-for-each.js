c3a43a52b14e2cd68bbcf787558626df6c7531e7
Jerson Uribe Montenegro <jerson.jhum@gmail.com>
2024-08-22 23:15:38 -0500

    Map method

--- server/01-for-each.js
+++ server/01-for-each.js
@@ -6,30 +6,3 @@ for (let index = 0; index < letters.length; index++) {
 }
 
 letters.forEach((item) => console.log(`ForEach: ${item}`));
-
-const tasks = [
-  {
-    title: 'Sacar basura',
-    date: '21/08/2024',
-    completed: false,
-  },
-  {
-    title: 'Limpiar arena de los gatos',
-    date: '22/08/2024',
-    completed: true,
-  },
-  {
-    title: 'Traer lo del desayuno',
-    date: '21/08/2024',
-    completed: false,
-  },
-];
-
-const app = document.getElementById('app');
-
-tasks.forEach((item) => {
-  app.innerHTML += `
-  <li>
-  <input type="checkbox" id="exampleCheckbox" name="exampleCheckbox" ${item.completed ? 'checked' : ''}>
-<label for="exampleCheckbox">${item.title}</label></li>`;
-});
